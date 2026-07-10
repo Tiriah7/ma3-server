b63941878181f6d4fba98f2880219b943c588d8d
Tiriah7 <ndunguchris49@gmail.com>
2026-07-10 12:22:10 +0300

    Update on firestoreClient variables on Railway

--- firestoreClient.js
+++ firestoreClient.js
@@ -1,10 +1,19 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Railway: credential comes from environment variable
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    // Local: credential comes from serviceAccountKey.json in the same folder
+    serviceAccount = require('./serviceAccountKey.json');
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
+
 module.exports = { admin, db };
